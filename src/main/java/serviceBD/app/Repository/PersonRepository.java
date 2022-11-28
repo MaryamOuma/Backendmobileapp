3e46eb95faa4aa350988462adb9d8cc90867fa69
MounaElHajji <111645562+MounaElHajji@users.noreply.github.com>
2022-11-28 22:20:08 +0000

    Update PersonRepository.java

--- src/main/java/serviceBD/app/Repository/PersonRepository.java
+++ src/main/java/serviceBD/app/Repository/PersonRepository.java
@@ -4,6 +4,6 @@ import serviceBD.app.Model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Integer>{
+public interface PersonRepository extends JpaRepository<Person, Long>{
 
 }
