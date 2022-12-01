a64b62ea4f10dd84bba5e1b559fd7e9422674194
MounaElHajji <111645562+MounaElHajji@users.noreply.github.com>
2022-12-01 20:09:48 +0000

    Update PersonRepository.java

--- src/main/java/serviceBD/app/Repository/PersonRepository.java
+++ src/main/java/serviceBD/app/Repository/PersonRepository.java
@@ -6,5 +6,11 @@ import serviceBD.app.Model.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long>{
+  @Query(nativeQuery = true, value = "SELECT * from Category as c JOIN Personne as p ON c.category_id = p.category_id")
+    List<Personne> findAllEmp();
+
+    @Query(nativeQuery = true, value = "SELECT * from Category as c JOIN Personne as p ON c.category_id = p.category_id where p.type like %:type% AND c.label like %:category% ")
+    List<Personne> findByCategoryAndType(String type, String category);
+
 
 }
