b0dc8eb911aff3c9362adff18af534eede2d6dd8
MounaElHajji <111645562+MounaElHajji@users.noreply.github.com>
2022-12-01 20:11:11 +0000

    Update PersonRepository.java

--- src/main/java/serviceBD/app/Repository/PersonRepository.java
+++ src/main/java/serviceBD/app/Repository/PersonRepository.java
@@ -1,16 +1,16 @@
 package serviceBD.app.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import serviceBD.app.Model.Person;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long>{
-  @Query(nativeQuery = true, value = "SELECT * from Category as c JOIN Personne as p ON c.category_id = p.category_id")
-    List<Personne> findAllEmp();
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    @Query(nativeQuery = true, value = "SELECT p.id, p.cin, p.first_name,p.city, p.function,p.image, p.last_name, p.tel, s.service_title, s.service_id from person as p JOIN service as s ON s.service_id = p.service_id")
+    List<Person> findAllEmp();
 
-    @Query(nativeQuery = true, value = "SELECT * from Category as c JOIN Personne as p ON c.category_id = p.category_id where p.type like %:type% AND c.label like %:category% ")
-    List<Personne> findByCategoryAndType(String type, String category);
-
-
+    @Query(nativeQuery = true, value = "SELECT p.id, p.cin, p.first_name,p.city, p.function,p.image, p.last_name, p.tel, s.service_title, s.service_id from person as p JOIN service as s ON s.service_id = p.service_id where p.function like %:function% AND s.service_title like %:service_title%")
+    List<Person> findByCategoryAndType(String function, String service_title);
 }
