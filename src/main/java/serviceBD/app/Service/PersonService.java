8f49e89f91554093f86df75fb316a5bd0a7db039
MounaElHajji <111645562+MounaElHajji@users.noreply.github.com>
2022-12-01 20:13:14 +0000

    Update PersonService.java

--- src/main/java/serviceBD/app/Service/PersonService.java
+++ src/main/java/serviceBD/app/Service/PersonService.java
@@ -1,8 +1,42 @@
 package serviceBD.app.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import serviceBD.app.Repository.PersonRepository;
+import serviceBD.app.Model.Person;
 
 @Service
 public class PersonService {
 
+    @Autowired
+    PersonRepository personRepository;
+
+    public List<Person> getAllEmployees() {
+        return personRepository.findAllEmp();
+    }
+
+    public List<Person> getEmployeeByCategory(String type, String category) {
+        // if (category != null) {
+        // return categoryRepository.findByCategory(category);
+        // }
+        return personRepository.findByCategoryAndType(type, category);
+    }
+
+    public Person getUserById(Long id) {
+        Optional<Person> personne = personRepository.findById(id);
+        Person a = null;
+        if (personne.isPresent()) {
+            a = personne.get();
+
+        } else {
+            throw new RuntimeException("Article doesn't exist for id: " + id);
+        }
+        return a;
+    }
+
+    
 }
