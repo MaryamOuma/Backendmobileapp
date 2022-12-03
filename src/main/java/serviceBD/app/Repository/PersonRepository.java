469aba4c309d84249063d0ca65ed41e4eb7e3242
Mouna <montalina833@gmail.com>
2022-12-03 21:45:08 +0000

    change repo

--- src/main/java/serviceBD/app/Repository/PersonRepository.java
+++ src/main/java/serviceBD/app/Repository/PersonRepository.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    @Query(nativeQuery = true, value = "SELECT p.id, p.cin, p.first_name,p.city, p.type_profil,p.image, p.last_name, p.tel, s.service_title, s.service_id from person as p JOIN service as s ON s.service_id = p.service_id")
+    @Query(nativeQuery = true, value = "SELECT p.id, p.cin, p.first_name,p.city, p.description, p.type_profil, p.image, p.last_name, p.tel, s.service_title, s.image from person as p JOIN service as s ON s.service_id = p.service_id")
     List<Person> findAllEmp();
 
-    @Query(nativeQuery = true, value = "SELECT p.id, p.cin, p.first_name,p.city, p.type_profil,p.image, p.last_name, p.tel, s.service_title, s.service_id from person as p JOIN service as s ON s.service_id = p.service_id where p.type_profil like %:type_profil% AND s.service_title like %:service_title%")
+    @Query(nativeQuery = true, value = "SELECT p.id, p.cin, p.first_name,p.city, p.description, p.type_profil, p.image, p.last_name, p.tel, s.service_title, s.image from person as p JOIN service as s ON s.service_id = p.service_id where p.type_profil like %:type_profil% AND s.service_title like %:service_title%")
     List<Person> findByCategoryAndType(String type_profil, String service_title);
 }
