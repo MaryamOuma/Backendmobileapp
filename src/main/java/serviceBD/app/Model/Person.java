e65f6d039db9515aa3071c75eed3a65614e26cf4
Nouhaila Ziyane <nouhaila.ziyane@uit.ac.ma>
2022-12-05 21:19:38 +0100

    cascade type

--- src/main/java/serviceBD/app/Model/Person.java
+++ src/main/java/serviceBD/app/Model/Person.java
@@ -41,7 +41,7 @@ public class Person {
     @Column(name = "description")
     private String description;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.DETACH )
     @JoinColumn(name = "service_id", nullable = false)
     private Service service;
 }
