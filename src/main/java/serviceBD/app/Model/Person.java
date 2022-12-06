80f07cd0e14bde49649627457fe5aa7439da4c3a
Kawtar Bekkali <kawtarbekkali1@gmail.com>
2022-12-06 15:22:03 +0100

    deleting the image attribute of person's model

--- src/main/java/serviceBD/app/Model/Person.java
+++ src/main/java/serviceBD/app/Model/Person.java
@@ -118,16 +118,6 @@ public class Person {
 
     @Column(name = "imageP")
     private String imageP;
-    @Column(name = "image")
-    private String image;
-
-    public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
 
 	@Column(name = "type_profil")
     private String typeProfil;
