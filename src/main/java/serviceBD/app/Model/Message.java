4e1f6571b21f3d66b189a8f4085f44e8c3437b9b
asmaeAssmougue <asmougue.asmae@gmail.com>
2023-01-04 20:22:33 +0000

    add messagerie

---
+++ src/main/java/serviceBD/app/Model/Message.java
@@ -0,0 +1,41 @@
+package serviceBD.app.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@DynamicUpdate
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int messageId;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.MERGE )
+    @JoinColumn(name = "message_to")
+    private Person messageTo;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.MERGE )
+    @JoinColumn(name = "message_from")
+    private Person messageFrom;
+
+    @Column(name = "message_text")
+    private String messageText;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+}
